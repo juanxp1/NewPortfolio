369621fb3872bee4c3817decf9d499013bfa3c12
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-10-06 20:45:53 -0300

    about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,26 +1,36 @@
 <template>
     <v-container>
         <v-sheet>
-            <h1 class="text-h1">Hola !</h1>
+            <h1 class="text-h2">¡Hemos llegado al final del portfolio! ¡Gracias por tomarte el tiempo para conocer mis
+                habilidades!</h1>
         </v-sheet>
         <v-divider class="border-opacity-0"></v-divider>
 
+        <v-sheet class="mt-10">
+            <h1 class="text-h4 font-weight-thin">Te invito a conocer un poco más sobre mí a través de mi <span
+                    class="bg-error">Historia.</span></h1>
+        </v-sheet>
+        <br>
+        <v-sheet class="text-end w-75 py-1 mt-10">
+            <v-col cols="auto">
+                <v-btn @click="redirectToHistory" size="x-large" elevation="10"
+                    class="text-h4 font-weight-thin custom-btn">¡Vamos allá!</v-btn>
+            </v-col>
+        </v-sheet>
+        <v-sheet>
+            <div class="espacio"></div>
+        </v-sheet>
     </v-container>
 </template>
+
   
 <script setup>
-
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-
-const redirectToProjects = () => {
-
-    router.push("/project");
+const redirectToHistory = () => {
+    window.location.href = "https://bianchi.vercel.app/";
 };
 </script>
   
-<style  scoped>
+<style scoped>
 .custom-btn {
     transition: background-color 0.4s, color 0.4s;
     background-color: #070707;
@@ -30,5 +40,9 @@ const redirectToProjects = () => {
 .custom-btn:hover {
     background-color: #ffff;
     color: #070707;
+}
+
+.espacio {
+    min-height: 50px;
 }
 </style>
